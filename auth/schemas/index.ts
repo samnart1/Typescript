403945d3396ba61ae55e3ab61fcc92e5510d9326
samnart1 <originalsamnart@gmail.com>
2025-01-10 15:14:20 +0100

    created some new files to make the auth app work

---
+++ auth/schemas/index.ts
@@ -0,0 +1,12 @@
+import * as z from "zod";
+
+export const LoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6, { message: "Invalid password" }),
+});
+
+export const RegisterSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6, { message: "Minimum 6 characters required" }),
+  name: z.string().min(1, { message: "Name is required" }),
+});
