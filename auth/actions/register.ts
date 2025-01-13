fcc4cad08abd36ef520089bef27284630d34e09e
samnart1 <originalsamnart@gmail.com>
2025-01-13 06:21:49 +0100

    modified register.ts, layout.tsx and package json

--- auth/actions/register.ts
+++ auth/actions/register.ts
@@ -1,13 +1,36 @@
 "use server";
+
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-  const validateFields = RegisterSchema.safeParse(values);
+  const validatedFields = RegisterSchema.safeParse(values);
 
-  if (!validateFields.success) {
-    return { error: "Invalid fields!" };
+  if (!validatedFields.success) {
+    return { error: "We have a problem guys, come out" };
   }
 
-  return { success: "Email sent!" };
+  const { name, password, email } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created!" };
 };
