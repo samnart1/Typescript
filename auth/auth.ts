e97bae7424242666dda147fc86b0442de732eda6
samnart1 <originalsamnart@gmail.com>
2025-01-14 04:10:23 +0100

    modified and created a middleware for the auth app

--- auth/auth.ts
+++ auth/auth.ts
@@ -1,9 +1,32 @@
 import NextAuth from "next-auth";
-import Github from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import { db } from "./lib/db";
+import authConfig from "./auth.config";
 
 export const {
   handlers: { GET, POST },
   auth,
+  signIn,
+  signOut,
 } = NextAuth({
-  providers: [Github],
+  callbacks: {
+    async session({ session, token }) {
+      console.log({ sessionToken: token, session });
+
+      if (session.user) {
+        session.user.customField = token.customField;
+      }
+      return session;
+    },
+
+    async jwt({ token }) {
+      console.log({ token });
+
+      return token;
+    },
+  },
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
