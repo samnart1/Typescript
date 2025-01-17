0d2747442c7ac01473490ce64fccc8508f10be74
samnart1 <originalsamnart@gmail.com>
2025-01-18 00:31:12 +0100

    created user-button

---
+++ auth/components/auth/user-button.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+
+export const UserButton = () => {
+  return <DropdownMenu>User Button</DropdownMenu>;
+};
