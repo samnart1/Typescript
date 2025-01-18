5d04ee3bf3e737cb9b16b9400e09ef52c744c937
samnart1 <originalsamnart@gmail.com>
2025-01-19 00:08:11 +0100

    created the client and server pages for the user

--- auth/auth.ts
+++ auth/auth.ts
@@ -62,17 +62,28 @@ export const {
       if (token.role && session.user) {
         session.user.role = token.role as UserRole;
       }
+
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+      }
+
       return session;
     },
 
     async jwt({ token }) {
       if (!token.sub) return token;
+
       const existingUser = await getUserById(token.sub);
+
       if (!existingUser) return token;
+
       token.role = existingUser.role;
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
+
       return token;
     },
   },
+
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
--- auth/components/auth/user-button.tsx
+++ auth/components/auth/user-button.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { FaUser } from "react-icons/fa";
+import { ExitIcon } from "@radix-ui/react-icons";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,7 +11,29 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { LogoutButton } from "./logout-button";
 
 export const UserButton = () => {
-  return <DropdownMenu>User Button</DropdownMenu>;
+  const user = useCurrentUser();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={user?.image || ""} />
+          <AvatarFallback className="bg-sky-500">
+            <FaUser className="text-white" />
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <LogoutButton>
+          <DropdownMenuItem>
+            <ExitIcon className="h-4 w-4 mr-2" />
+            Logout
+          </DropdownMenuItem>
+        </LogoutButton>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
