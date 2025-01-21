1e8ca48b610deecccc8ef2a2b28822f53c2048d3
samnart1 <originalsamnart@gmail.com>
2025-01-21 05:33:45 +0100

    modification

--- auth/app/(protected)/client/page.tsx
+++ auth/app/(protected)/client/page.tsx
@@ -3,7 +3,7 @@
 import UserInfo from "@/components/user-info";
 import { useCurrentUser } from "@/hooks/use-current-user";
 
-const ClientPage = async () => {
+const ClientPage = () => {
   const user = useCurrentUser();
 
   return <UserInfo user={user} label="📱 Client component" />;
