d354d45e00493b10176252f74b208b8d8a3ba720
samnart1 <originalsamnart@gmail.com>
2025-01-25 22:17:19 +0100

    asdf

--- airbnb/app/components/Container.tsx
+++ airbnb/app/components/Container.tsx
@@ -2,5 +2,13 @@ interface ContainerProps {
   children: React.ReactNode;
 }
 export default function Container({ children }: ContainerProps) {
-  return <div>{children}</div>;
+  return (
+    <div
+      className="
+      max-w-[2520px]
+    "
+    >
+      {children}
+    </div>
+  );
 }
