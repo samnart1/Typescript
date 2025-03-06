2364f85156b81f48a858f2260f465df38b9b84e4
samnart1 <originalsamnart@gmail.com>
2025-03-06 16:24:10 +0100

    started a simple hangman project in react

---
+++ hangman/src/HangmanWord.tsx
@@ -0,0 +1,37 @@
+export function HangmanWord() {
+    const word = "test";
+    const guessedLetters = ["t"];
+
+    return (
+        <div
+            style={{
+                display: "flex",
+                gap: ".25em",
+                fontSize: "6rem",
+                fontWeight: "bold",
+                textTransform: "uppercase",
+                fontFamily: "monospace",
+                margin: "30px",
+            }}
+        >
+            {word.split("").map((letter, index) => (
+                <span
+                    key={index}
+                    style={{
+                        borderBottom: ".1em solid black",
+                    }}
+                >
+                    <span
+                        style={{
+                            visibility: guessedLetters.includes(letter)
+                                ? "visible"
+                                : "hidden",
+                        }}
+                    >
+                        {letter}
+                    </span>
+                </span>
+            ))}
+        </div>
+    );
+}
