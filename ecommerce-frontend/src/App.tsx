1339844bcd4e31fbb036d0e9b7d6d2b836243ad0
samnart1 <originalsamnart@gmail.com>
2025-04-04 01:34:17 +0200

    checkout page ...

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -10,6 +10,7 @@ import Cart from "./components/cart/Cart";
 import Login from "./components/auth/Login";
 import PrivateRoute from "./components/PrivateRoute";
 import Register from "./components/auth/Register";
+import Checkout from "./components/checkout/Checkout";
 
 function App() {
     return (
@@ -23,6 +24,8 @@ function App() {
                     <Route path="/about" element={<About />} />
                     <Route path="/contact" element={<Contact />} />
                     <Route path="/cart" element={<Cart />} />
+                    <Route path="/checkout" element={<Checkout />} />
+
                     <Route path="/" element={<PrivateRoute publicPage />}>
                         <Route path="/login" element={<Login />} />
                         <Route path="/register" element={<Register />} />
---
+++ ecommerce-frontend/src/components/checkout/Checkout.tsx
@@ -0,0 +1,25 @@
+import { Step, StepLabel, Stepper } from "@mui/material";
+import { useState } from "react";
+import AddressInfo from "./AddressInfo";
+
+export const Checkout = () => {
+    const [activeStep, setActiveStep] = useState(0);
+
+    const steps = ["Address", "Payment Method", "Order Summary", "Payment"];
+
+    return (
+        <div className="py-14 min-h-[calc(100vh-100px)]">
+            <Stepper activeStep={activeStep} alternativeLabel>
+                {steps.map((label, index) => (
+                    <Step key={index}>
+                        <StepLabel>{label}</StepLabel>
+                    </Step>
+                ))}
+            </Stepper>
+
+            <div className="mt-5">{activeStep === 0 && <AddressInfo />}</div>
+        </div>
+    );
+};
+
+export default Checkout;
